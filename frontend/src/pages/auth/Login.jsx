8d66750fd90ad4dd25c8cffe90ac8f4b26b772a7
AmitSahoo7 <amit.sahoo757@gmail.com>
2025-06-09 23:48:27 +0530

    Updated Design

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
 import "./auth.css";
 import { Link, useNavigate } from "react-router-dom";
-const Login = () =>{
-    return (
-        
-    <div className="auth-page" >
-    <div className="auth-form">
-    <h2>Login</h2>
-    <form>
-    <label htmlFor="email" >Email</label>
-    <input type="email" required />
-    <label htmlFor="password">Password</label>
-    <input type="password" required /> 
-    <button className="common-btn" >Login</button>
-    </form>
-    <p>
-        Don't have an account? <Link to="/register">Register</Link>
-    </p>
+const Login = () => {
+  return (
+    <div className="auth-page">
+      <div className="auth-form">
+        <h2>Login</h2>
+        <form>
+          <label htmlFor="email">Email</label>
+          <input type="email" required />
+          <label htmlFor="password">Password</label>
+          <input type="password" required />
+          <button className="common-btn">Login</button>
+        </form>
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
     </div>
-    </div>
-    );  
+  );
 };
 export default Login;
-
