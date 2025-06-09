e332407c2e978bdf8649401faf9b9de9c119a67f
AmitSahoo7 <amit.sahoo757@gmail.com>
2025-06-09 22:53:00 +0530

    Started frontend with auth pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Header from "./components/header/Header";
+import Verify from "./pages/auth/Verify";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify" element={<Verify />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div className="home">
+        <div className="home-content">
+          <h1>Welcome to our E-learning Platform</h1>
+          <p>Learn, Grow, Excel</p>
+        </div>
+      </div>
+    
+    </div>
+  );
+};
+
+export default Home;
